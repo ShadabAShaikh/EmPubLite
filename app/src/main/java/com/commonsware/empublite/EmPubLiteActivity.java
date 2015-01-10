c8436965f5eb026b5ac2f7ea0a394e29cc1989ae
ShadabAShaikh <shadab.shaikh@gmail.com>
2015-01-10 17:13:27 -0500

    2015.01.10.17.13

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -3,14 +3,24 @@ package com.commonsware.empublite;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class EmPubLiteActivity extends Activity {
+    private ViewPager pager=null;
+    private ContentsAdapter adapter=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        pager=(ViewPager)findViewById(R.id.pager);
+        adapter=new ContentsAdapter(this);
+        pager.setAdapter(adapter);
+        findViewById(R.id.progressBar1).setVisibility(View.GONE);
+        findViewById(R.id.pager).setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -26,10 +36,14 @@ public class EmPubLiteActivity extends Activity {
                 return(true);
             case R.id.about:
                 Intent i=new Intent(this, SimpleContentActivity.class);
+                i.putExtra(SimpleContentActivity.EXTRA_FILE,
+                        "file:///android_asset/misc/about.html");
                 startActivity(i);
                 return(true);
             case R.id.help:
                 i=new Intent(this, SimpleContentActivity.class);
+                i.putExtra(SimpleContentActivity.EXTRA_FILE,
+                        "file:///android_asset/misc/help.html");
                 startActivity(i);
                 return(true);
         }
